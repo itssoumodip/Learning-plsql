805463344a67808e4fcb9aa9fa4f27f3d98b2150
itssoumodip <soumodipddas2005@gmail.com>
2025-02-16 20:30:16 +0530

    fixing the errors

--- part1/grater.sql
+++ part1/grater.sql
@@ -6,9 +6,9 @@ BEGIN
     a:=&a;
     b:=&b;
     if(a>b) then 
-    dbms_output.put_line('1st No '||a||'is Greater');
+    dbms_output.put_line('1st No '||a||' is Greater');
     elsif(b>a) then
-    dbms_output.put_line('2nd No '||b||'is Greater');
+    dbms_output.put_line('2nd No '||b||' is Greater');
     else 
     dbms_output.put_line('All are Equal');
     end if;
